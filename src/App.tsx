f332403817428814694efd010804370ca64835af
seika <504524274@qq.com>
2023-11-08 02:53:27 +0900

    fix: Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../src/pages/Home';
 import Detail from '../src/pages/Detail';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
